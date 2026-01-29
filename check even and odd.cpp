9f224c1c6186b6ae4db8a2d42a8823b87d604cc5
joanpmorre <joanpmorre@gmail.com>
2026-01-29 22:39:42 +0800

    Add files via upload

---
+++ check even and odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << "The number is Even." << endl;
+    } else {
+        cout << "The number is Odd." << endl;
+    }
+
+    return 0;
+}
